df4d6305f197fbb5f7a14a5e106ec05845fcb13e
Farasaa <saad.ahmed.farag.91@gmail.com>
2024-09-07 15:16:02 +0300

    Updated the project to be fully responsive

--- src/components/ImagesList.tsx
+++ src/components/ImagesList.tsx
@@ -18,7 +18,7 @@ export default function ImageList({images}: ImageProps){
     })
 
     return(
-        <div className="grid grid-cols-5 gap-4 justify-items-center items-center mt-4 mb-4">
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-4 place-items-center min-h-screen place-content-center">
             {renderImages}
             </div>
     )
